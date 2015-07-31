aae7c7b06e69c01f016084189f6fb308f761a8ef
git <yiecao1993@yeah.net>
2015-07-31 16:57:09 +0800

    update:  mysql

--- db_sql/create-database.sql
+++ db_sql/create-database.sql
@@ -8,14 +8,14 @@ CREATE TABLE parameters
   para_id int unsigned not null auto_increment primary key,
   para_name CHAR (32) not null,
   para_description VARCHAR (255)
-) TYPE = InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE para_values
 (
   para_id int unsigned not null REFERENCES parameters(para_id),
   para_value_id int unsigned not null auto_increment primary key,
   para_value_name char (32) not null
-) TYPE = InnoDB;
+) ENGINE = InnoDB;
 
 create table items
 (
@@ -26,21 +26,21 @@ create table items
   item_create_time datetime not null,
   item_description varchar(255),
   item_type_id int unsigned not null REFERENCES para_values(para_value_id),
-  item_state enum('PROCESSING', 'FINISH') not null DEFAULT 'PROCESSING',
-) TYPE = InnoDB;
+  item_state enum('PROCESSING', 'FINISH') not null DEFAULT 'PROCESSING'
+) ENGINE = InnoDB;
 
 CREATE TABLE item_follow_marks
 ( 
-  item_follow_mark_id int unsigned not null auto_increment primary key
+  item_follow_mark_id int unsigned not null auto_increment primary key,
   item_id int unsigned not null REFERENCES items(item_id),
-  item_follow_mark VARCHAR not null 
-) TYPE = InnoDB;
+  item_follow_mark VARCHAR(255) not null 
+) ENGINE = InnoDB;
 
 CREATE TABLE roles
 (
   role_id int unsigned not null auto_increment PRIMARY KEY ,
-  role_name CHAR (32) NOT NULL ,
-) TYPE = InnoDB;
+  role_name CHAR (32) NOT NULL
+) ENGINE = InnoDB;
 
 CREATE TABLE role_priv
 (
@@ -48,8 +48,7 @@ CREATE TABLE role_priv
   role_id int unsigned NOT NULL REFERENCES roles(role_id),
   role_priv_name char (32) not null,
   role_priv_value enum('YES', 'NO') NOT NULL DEFAULT 'NO'
-
-) TYPE = InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE users
 (
@@ -58,15 +57,15 @@ CREATE TABLE users
   user_passwd CHAR (40) not NULL ,
   role_id int unsigned NOT NULL REFERENCES roles(role_id),
   user_mail CHAR (100) not null
-)  TYPE = InnoDB;
+)  ENGINE = InnoDB;
 
 CREATE TABLE auto_notify
 (
   item_id int unsigned not null REFERENCES items(item_id),
   auto_date datetime not null,
   auto_type enum ('ONCE', 'DAILY', 'WEEKLY', 'MONTHLY', 'QUARTERLY', 'YEARLY') NOT NULL DEFAULT 'ONCE',
-  user_id int unsigned not null REFERENCES users(user_ic)
-) TYPE = InnoDB;
+  user_id int unsigned not null REFERENCES users(user_id)
+) ENGINE = InnoDB;
 
 create table logs
 (
@@ -74,7 +73,7 @@ create table logs
   item_id int unsigned not null REFERENCES items(item_id),
   log_changer_id int unsigned not null REFERENCES users(user_id),
   log_time datetime not null
-) TYPE = InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE log_fields
 (
@@ -82,7 +81,7 @@ CREATE TABLE log_fields
   log_field_name CHAR (32) not NULL,
   log_field_old CHAR (32) NOT NULL,
   log_field_new CHAR (32) NOT NULL
-) TYPE = InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE admin_logs
 (
@@ -90,7 +89,7 @@ CREATE TABLE admin_logs
   admin_log_time datetime not null,
   admin_log_object char (32) not null,
   admin_log_object_id int unsigned not null
-) TYPE = InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE admin_log_fileds
 (
@@ -98,7 +97,7 @@ CREATE TABLE admin_log_fileds
   admin_log_field_name CHAR (32) not NULL,
   admin_log_field_old CHAR (32) NOT NULL,
   admin_log_field_new CHAR (32) NOT NULL
-) TYPE = InnoDB;
+) ENGINE = InnoDB;
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 on shadow.*
--- db_sql/properties.sql
+++ db_sql/properties.sql
@@ -1,11 +1,13 @@
 use shadow;
 
-INSERT INTO admin VALUES ('admin', 'admin');
 
 INSERT INTO parameters VALUES ('', 'item_type', 'Item`s types');
 INSERT INTO para_values VALUES ('1', '', 'common');
 INSERT INTO para_values VALUES ('1', '', 'special');
 
+
+INSERT INTO roles VALUES('', 'common');
+INSERT INTO role_priv VALUES('', '1', '日常事务', 'YES');
+INSERT INTO role_priv VALUES('', '1', '生产问题', 'YES');
 INSERT INTO users VALUES('', 'yiyd', sha1('123456'), '1', 'yiyd@qq.com');
 INSERT INTO users VALUES('', 'yiyd1', sha1('123456'), '1', 'yiyd@qq.com');
-INSERT into roles VALUES('', 'common', 'YES', 'YES', 'YES', 'YES', 'YES', 'YES');
