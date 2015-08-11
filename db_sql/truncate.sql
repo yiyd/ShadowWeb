9b796b89d7673a7d90748f9ddc4d5af9e79cb2b4
git <yiecao1993@yeah.net>
2015-08-11 17:18:32 +0800

    update

---
+++ db_sql/truncate.sql
@@ -0,0 +1,16 @@
+use shadow;
+
+truncate table parameters;
+truncate table para_values;
+truncate table users;
+truncate table user_access;
+truncate table roles;
+truncate table privileges;
+truncate table role_priv;
+truncate table logs;
+truncate table log_fields;
+truncate table admin_logs;
+truncate table admin_log_fields;
+truncate table items;
+truncate table item_follow_marks;
+truncate table auto_notify;
